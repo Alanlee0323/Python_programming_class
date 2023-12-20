84d1aea95b35fa8fcfed22db9d369ac8bf2413ac
YU-LUN-LEE <95911604+Alanlee0323@users.noreply.github.com>
2023-12-20 16:03:39 +0800

    update

--- week4/practice4_2.py
+++ week4/practice4_2.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # 載入數據集
-file_path = 'movie.csv'
+file_path = 'C:\\Users\\alana\\Dropbox\\Github\\Python_programming_class\\week4\\movie.csv'
 movie_data = pd.read_csv(file_path)
 
 # 顯示前五行的數據
--- week4/practice4_4.py
+++ week4/practice4_4.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # 載入數據集
-file_path = 'movie.csv'
+file_path = 'C:\\Users\\alana\\Dropbox\\Github\\Python_programming_class\\week4\\movie.csv'
 movie_data = pd.read_csv(file_path)
 
 # 顯示前五行的數據
